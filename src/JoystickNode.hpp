d4b04d92622f02786de0239d86d93c6ea7cf1e24
41-aaa <LNunez@student.raeschools.org>
2026-01-04 11:51:00 -0500

    Add files via upload

---
+++ src/JoystickNode.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+class JoystickNode : public CCMenu {
+    protected:
+        bool init() override;
+
+        bool ccTouchBegan(CCTouch *touch, CCEvent *event) override;
+        void ccTouchEnded(CCTouch *touch, CCEvent *event) override;
+        void ccTouchMoved(CCTouch *touch, CCEvent *event) override;
+        void ccTouchCancelled(CCTouch *touch, CCEvent *event) override;
+
+        void draw() override;
+        void registerWithTouchDispatcher() override;
+
+        CCSprite *m_bg = nullptr;
+        CCSprite *m_center = nullptr;
+    public:
+        static JoystickNode *create();
+        void handleInput(GJBaseGameLayer *layer, CCPoint input, CCPoint old);
+        void fakePosition();
+        
+        bool m_twoPlayer = false;
+        CCPoint m_currentInput = {0, 0};
+};
